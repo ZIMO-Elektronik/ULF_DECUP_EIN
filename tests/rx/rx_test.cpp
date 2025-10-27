bca10541942515346561ae8a7cfa40efe938810d
Vincent Hamp <vincent.hamp@higaski.at>
2025-10-27 14:59:45 +0100

    chore: workaround for Espressif's misuse of CMake (espressif/esp-idf#17773)

--- tests/rx/rx_test.cpp
+++ tests/rx/rx_test.cpp
@@ -5,65 +5,65 @@ RxTest::RxTest() {}
 
 RxTest::~RxTest() {}
 
-RxTest& RxTest::Zsu(std::filesystem::path path) {
+RxTest* RxTest::Zsu(std::filesystem::path path) {
   _zsu = zsu::read(path);
-  return *this;
+  return this;
 }
 
-RxTest& RxTest::ZsuPreamble(size_t count) {
+RxTest* RxTest::ZsuPreamble(size_t count) {
   for (auto i{0uz}; i < count; ++i)
     _mock.receive(std::to_underlying(count % 2uz ? decup::Command::Preamble0
                                                  : decup::Command::Preamble1));
-  return *this;
+  return this;
 }
 
-RxTest& RxTest::ZsuDecoderId(uint8_t decoder_id) {
+RxTest* RxTest::ZsuDecoderId(uint8_t decoder_id) {
   EXPECT_CALL(_mock, transmit(_)).Times(AnyNumber());
   EXPECT_CALL(_mock, transmit(ElementsAre(decoder_id)))
     .WillOnce(Return(2u))
     .RetiresOnSaturation();
   auto it{std::ranges::find_if(_zsu.firmwares, [this](auto&& fw) {
-    return _mock.receive(fw.id) == ulf::decup_ein::ack;
+    return _mock.receive(static_cast<uint8_t>(fw.id)) == ulf::decup_ein::ack;
   })};
   if (it != cend(_zsu.firmwares)) _fw = *it;
-  return *this;
+  return this;
 }
 
-RxTest& RxTest::ZsuBlockCount() {
+RxTest* RxTest::ZsuBlockCount() {
   auto block_count{static_cast<uint8_t>(size(_fw.bin) / 256u + 8u - 1u)};
   EXPECT_CALL(_mock, transmit(ElementsAre(block_count)))
     .WillOnce(Return(1u))
     .RetiresOnSaturation();
   _mock.receive(block_count);
-  return *this;
+  return this;
 }
 
-RxTest& RxTest::ZsuSecurityByte1() {
+RxTest* RxTest::ZsuSecurityByte1() {
   EXPECT_CALL(_mock, transmit(ElementsAre(0x55u)))
     .WillOnce(Return(1u))
     .RetiresOnSaturation();
   _mock.receive(0x55u);
-  return *this;
+  return this;
 }
 
-RxTest& RxTest::ZsuSecurityByte2() {
+RxTest* RxTest::ZsuSecurityByte2() {
   EXPECT_CALL(_mock, transmit(ElementsAre(0xAAu)))
     .WillOnce(Return(1u))
     .RetiresOnSaturation();
   _mock.receive(0xAAu);
-  return *this;
+  return this;
 }
 
-RxTest& RxTest::ZsuBlocks() {
+RxTest* RxTest::ZsuBlocks() {
   EXPECT_CALL(_mock, transmit(_))
     .Times(Exactly(836))
     .WillRepeatedly(Return(2u));
-  auto count{0uz};
+  uint8_t count{0u};
   for (auto chunk : _fw.bin | std::views::chunk(decup::decoder_id2data_size(
                                 static_cast<uint8_t>(_fw.id)))) {
     _mock.receive(count);
     std::ranges::for_each(chunk, [this](uint8_t byte) { _mock.receive(byte); });
     _mock.receive(count++ ^ decup::exor(chunk));
   }
-  return *this;
+  return this;
 }
--- tests/rx/rx_test.hpp
+++ tests/rx/rx_test.hpp
@@ -12,15 +12,14 @@ struct RxTest : ::testing::Test {
   RxTest();
   virtual ~RxTest();
 
-  RxTest& Zsu(std::filesystem::path path);
-  RxTest& ZsuPreamble(size_t count);
-  RxTest& ZsuDecoderId(uint8_t decoder_id);
-  RxTest& ZsuBlockCount();
-  RxTest& ZsuSecurityByte1();
-  RxTest& ZsuSecurityByte2();
-  RxTest& ZsuBlocks();
+  RxTest* Zsu(std::filesystem::path path);
+  RxTest* ZsuPreamble(size_t count);
+  RxTest* ZsuDecoderId(uint8_t decoder_id);
+  RxTest* ZsuBlockCount();
+  RxTest* ZsuSecurityByte1();
+  RxTest* ZsuSecurityByte2();
+  RxTest* ZsuBlocks();
 
-protected:
   NiceMock<RxMock> _mock;
   zsu::File _zsu;
   zsu::Firmware _fw;
