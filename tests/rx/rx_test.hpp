837ac880856ee33e6d06ff43de784e69086fcb1a
Vincent Hamp <vincent.hamp@higaski.at>
2026-02-02 08:54:29 +0100

    chore: style changes

--- tests/rx/rx_test.hpp
+++ tests/rx/rx_test.hpp
@@ -13,22 +13,22 @@ struct RxTest : ::testing::Test {
   RxTest();
   virtual ~RxTest();
 
-  RxTest* Zsu(std::filesystem::path path);
-  RxTest* ZsuPreamble(size_t count);
-  RxTest* ZsuDecoderId(uint8_t decoder_id);
-  RxTest* ZsuBlockCount();
-  RxTest* ZsuSecurityByte1();
-  RxTest* ZsuSecurityByte2();
-  RxTest* ZsuBlocks();
+  RxTest& Zsu(std::filesystem::path path);
+  RxTest& ZsuPreamble(size_t count);
+  RxTest& ZsuDecoderId(uint8_t decoder_id);
+  RxTest& ZsuBlockCount();
+  RxTest& ZsuSecurityByte1();
+  RxTest& ZsuSecurityByte2();
+  RxTest& ZsuBlocks();
 
-  RxTest* Zpp(std::filesystem::path path);
-  RxTest* ZppPreamble(size_t count);
-  RxTest* ZppDecoderId();
-  RxTest* ZppCvRead(uint16_t cv);
-  RxTest* ZppCvWrite(uint16_t cv, uint8_t val);
-  RxTest* ZppFlashErase();
-  RxTest* ZppFlashWrite();
-  RxTest* ZppCRCorXOR();
+  RxTest& Zpp(std::filesystem::path path);
+  RxTest& ZppPreamble(size_t count);
+  RxTest& ZppDecoderId();
+  RxTest& ZppCvRead(uint16_t cv);
+  RxTest& ZppCvWrite(uint16_t cv, uint8_t val);
+  RxTest& ZppFlashErase();
+  RxTest& ZppFlashWrite();
+  RxTest& ZppCRCorXOR();
 
   NiceMock<RxMock> _mock;
   zsu::File _zsu;
